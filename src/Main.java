fbb682ddb7e6bcee408dc3ad8a929214242e7635
Deepu Yadav <deepu.ug20@nsut.ac.in>
2024-07-10 00:24:15 +0530

    added instructions for playing game

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,14 @@ public class Main {
         OutputHandlerInterface outputHandler = new OutputHandlerImplementation();
 
         System.out.println("Game is Loading...");
+        outputHandler.newLine();
+        outputHandler.printString("Instructions: ");
+        outputHandler.newLine();
+        outputHandler.printString("Game can be played in two modes custom mode and default mode");
+        outputHandler.printString("Custom mode: User will give attack, health, strength and name as input to game");
+        outputHandler.printString("Default mode: Game will randomly generate values for attack, health and strength.");
+        outputHandler.printString("*************************************************************************");
+        outputHandler.newLine();
         GameInitializer gameInitializer = new GameInitializer();
         gameInitializer.initializeGame();
     }
