769979e770867b36ad6a35b919fa328f7392a466
Deepu Yadav <deepu.ug20@nsut.ac.in>
2024-07-09 22:55:47 +0530

    completed CustomInputHandler class

--- src/GameLogic/PlayerInitializer/CustomPlayerInitializer.java
+++ src/GameLogic/PlayerInitializer/CustomPlayerInitializer.java
@@ -1,5 +1,27 @@
 package GameLogic.PlayerInitializer;
 
-public class CustomPlayerInitializer {
+import Utility.InputHandler.InputHandler;
+import Utility.InputHandler.InputHandlerInterface;
+import model.Player;
 
+public class CustomPlayerInitializer {
+    InputHandlerInterface inputHandler = new InputHandler();
+    Player player;
+
+    Player playerInitializer(){
+        System.out.println("Enter name of the player: ");
+        String name = inputHandler.inputString();
+
+        System.out.println("Enter health of the player: ");
+        long health = inputHandler.integerInput();
+
+        System.out.println("Enter strength of the player: ");
+        long strength = inputHandler.integerInput();
+
+        System.out.println("Enter attack of the player: ");
+        long attack = inputHandler.integerInput();
+
+        player = new Player(name, health, strength, attack);
+        return player;
+    }
 }
