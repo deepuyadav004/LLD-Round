294279369dd01b4248b9e9cb0f612d83000294c0
Deepu Yadav <deepu.ug20@nsut.ac.in>
2024-07-09 22:04:24 +0530

    added comments for more readability

--- src/Utility/RandomNumberGenerator.java
+++ src/Utility/RandomNumberGenerator.java
@@ -5,14 +5,22 @@ import java.util.Random;
 public class RandomNumberGenerator {
     Random random = new Random();
 
+    /*
+    * This function will generate random number between 1 and limit inclusive.
+    * This function will be used when user provide a upper limit of attack, strength, health
+    * */
     public long generateRandomNumber(long limit){
         if(limit < 1){
             throw new IllegalArgumentException("Limit cannot be negative");
         }
-        return random.nextLong(limit+1);
+        return random.nextLong(limit)+1;
     }
 
+    /*
+    * By default this function will be used.
+    * It will generate random number between 1 and 1000 inclusive
+    * */
     public long generateRandomNumber(){
-        return random.nextLong(1001);
+        return random.nextLong(1000)+1;
     }
 }
